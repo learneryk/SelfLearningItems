a0b852d0d7d419ed9901261014201602ff98a4d6
learneryk <timtom2023@gmail.com>
2026-05-21 16:18:04 +0530

    some changes

--- nodeJS/server.js
+++ nodeJS/server.js
@@ -4,11 +4,18 @@ const path = require("path")
 const dirName = __dirname
 const fileName = __filename
 
-console.log(`directory name: ${dirName}`)
-console.log(`using path.dirname(): ${path.dirname(dirName)}`)
-console.log(`using path.basename(): ${path.basename(dirName)}`)
-console.log(`using path.parse(): ${path.parse(fileName)}`)
-console.log(path.parse(__filename)) //formatted Text gives wrong output 
+console.log(`dirName: ${dirName}`)
+console.log(`fileName: ${fileName}`)
+
+console.log(`using path.dirname(dirName): ${path.dirname(dirName)}`)
+console.log(`using path.dirname(fileName): ${path.dirname(fileName)}`)
+
+console.log(`using path.basename(dirName): ${path.basename(dirName)}`)
+console.log(`using path.basename(fileName): ${path.basename(fileName)}`)
+
+console.log(path.parse(fileName))
+console.log(`using path.extname(): ${path.extname(fileName)}`)
+console.log(path.parse(dirName)) //formatted Text gives wrong output 
 
 
 // That's how it is
@@ -19,3 +26,5 @@ const myObject = {
 
 console.log(`${myObject}`) // returns -> [object Object]
 console.log(myObject) // returns -> { name: 'afsal', age: 22 }
+
+
